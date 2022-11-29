0db96051f256f8e1e4e45d6b415a2f38e25f4c82
CaterinaBi <catherinebonan@gmail.com>
2022-11-29 14:37:32 +0000

    Fixes typo

--- clean_tabular_data.py
+++ clean_tabular_data.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
 pd.set_option('display.max_columns', None)
-products_df = pd.read_csv("products.cvs", sep="|")
+products_df = pd.read_csv("products.csv", sep="|")
+
+# display first lines
+products_df.head()
