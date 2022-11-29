9efcae629d12c30b0bc5c35ddcae48ae9ed3667c
CaterinaBi <catherinebonan@gmail.com>
2022-11-29 14:52:53 +0000

    Completes obtain_tabular_data() method

--- clean_tabular_data.py
+++ clean_tabular_data.py
@@ -1,7 +1,19 @@
 import pandas as pd
 
-pd.set_option('display.max_columns', None)
-products_df = pd.read_csv("products.csv", sep=",")
+def obtain_tabular_data(file_path: str, line_terminator: str = ',') -> pd.DataFrame:
+    '''A function that imports data from a .cvs file to a pandas data frame, and deletes all incomplete rows.
+    Args:
+        file_path (str): path from where the data is to be imported
+        line_terminator (str): line terminator of the .cvs file, by default a comma (',').
+    Returns:
+        ^d.DataFrame: data frame of all complete contents from the .cvs file
+    '''
 
-# display first lines
-products_df.head()
+    products_df = pd.read_csv("products.csv", sep=",")
+    products_df.dropna() # deletes columns where at least 1 item is missing
+
+    return products_df
+
+if __name__ == "__main__":
+    file_path = "tabular_data/products.csv"
+    line_terminator = "\n"
