3e933e0f2d12d886c700aca2d4d31d22742f822f
CaterinaBi <catherinebonan@gmail.com>
2022-11-29 14:38:41 +0000

    Fixes separator

--- clean_tabular_data.py
+++ clean_tabular_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 pd.set_option('display.max_columns', None)
-products_df = pd.read_csv("products.csv", sep="|")
+products_df = pd.read_csv("products.csv", sep=",")
 
 # display first lines
 products_df.head()
