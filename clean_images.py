4ec97c6bd41d4eac7be83c88903966123b2f3fd6
CaterinaBi <catherinebonan@gmail.com>
2022-12-01 20:31:54 +0000

    Modifies function

--- clean_images.py
+++ clean_images.py
@@ -1,11 +1,11 @@
 from PIL import Image
 import os
 
-def resize_image(final_size, im):
-    size = im.size
+def resize_image(final_size, image):
+    size = image.size
     ratio = float(final_size) / max(size)
     new_image_size = tuple([int(x*ratio) for x in size])
-    im = im.resize(new_image_size, Image.ANTIALIAS)
-    new_im = Image.new("RGB", (final_size, final_size))
-    new_im.paste(im, ((final_size-new_image_size[0])//2, (final_size-new_image_size[1])//2))
-    return new_im
+    image = image.resize(new_image_size, Image.ANTIALIAS)
+    new_image = Image.new("RGB", (final_size, final_size))
+    new_image.paste(image, ((final_size-new_image_size[0])//2, (final_size-new_image_size[1])//2))
+    return new_image
