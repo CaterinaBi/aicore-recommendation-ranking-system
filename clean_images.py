f04444346610fd10d51ebd7fe5e8eea4f1024875
CaterinaBi <catherinebonan@gmail.com>
2022-12-01 20:19:15 +0000

    Deletes file, populates resize_image() function

---
+++ clean_images.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import os
+
+def resize_image(final_size, im):
+    size = im.size
+    ratio = float(final_size) / max(size)
+    new_image_size = tuple([int(x*ratio) for x in size])
+    im = im.resize(new_image_size, Image.ANTIALIAS)
+    new_im = Image.new("RGB", (final_size, final_size))
+    new_im.paste(im, ((final_size-new_image_size[0])//2, (final_size-new_image_size[1])//2))
+    return new_im
